dec73a073c2cc92a9853d13768132d125f30b7d7
DongChaofeng <s654632396@hotmail.com>
2019-09-25 19:30:49 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# go-study
+study for golang
--- learn/rabbitmq/producer.go
+++ learn/rabbitmq/producer.go
@@ -12,7 +12,7 @@ func failOnError(err error, msg string) {
 }
 
 func main() {
-	conn, err := amqp.Dial("amqp://dongchaofeng:123456@localhost:5672/")
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
 	failOnError(err, "Failed to connect to RabbitMQ")
 	defer conn.Close()
 
