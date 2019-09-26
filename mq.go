8985b6a8132159022b754638bd76bdbf689b41f1
haruharu <s654632396@hotmail.com>
2019-09-26 22:10:23 +0800

    fix mq.go

--- mq.go
+++ mq.go
@@ -20,20 +20,21 @@ type Task struct {
 
 func (t *Task) run() {
 	sleep := rand.Intn(1000)
+	// simulate dealing task
 	time.Sleep(time.Duration(sleep) * time.Millisecond)
+	// change task status to completed
 	t.Status = "Completed"
 }
 
 func main() {
 	var wg sync.WaitGroup
-	worknum := WorkNum
-	wg.Add(worknum)
-
+	// channel of task Queue
 	var ch chan *Task = make(chan *Task, 10)
-	startWorkers(worknum, wg, ch)
+	startWorkers(WorkNum, &wg, ch)
 
 	rand.Seed(time.Now().UnixNano())
 
+	// producer
 	for i := 0; i < 30; i++ {
 		ch <- &Task{
 			ID:       int32(i),
@@ -50,18 +51,21 @@ func main() {
 	fmt.Println("all task done.")
 }
 
-func startWorkers(num int, wg sync.WaitGroup, ch chan *Task) {
-	for i := 0; i <= num; i++ {
-		fmt.Println("start worker :", i+1)
-		go func(wg sync.WaitGroup, ch chan *Task) {
+func startWorkers(num int, wg *sync.WaitGroup, ch chan *Task) {
+	for i := 1; i <= num; i++ {
+		wg.Add(1)
+		fmt.Println("start worker :", i)
+		go func(wg *sync.WaitGroup, ch chan *Task, workId int16) {
 			defer wg.Done()
 			//
 			for t := range ch {
+				t.Status = "prepare"
 				// comsumer
 				// do some thing
 				fmt.Printf("recv task(%d). \n", t.ID)
 				t.run()
+				fmt.Printf("done task(%d). \n", t.ID)
 			}
-		}(wg, ch)
+		}(wg, ch, int16(i))
 	}
 }
