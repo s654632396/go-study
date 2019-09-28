f9e0d202486b50a6ec12c06d31481c6229a8503b
haruharu <s654632396@hotmail.com>
2019-09-29 00:10:35 +0800

    update

--- go.mod
+++ go.mod
@@ -6,5 +6,10 @@ require (
 	github.com/bradfitz/gomemcache v0.0.0-20190913173617-a41fca850d0b
 	github.com/jinzhu/gorm v1.9.10
 	github.com/s654632396/doublelist v0.0.0-20190925111446-8735c5d97a04
+	github.com/s654632396/pkg/adt v0.0.0-20190928155848-fa98018e95fa
 	github.com/streadway/amqp v0.0.0-20190827072141-edfb9018d271
 )
+
+// replace github.com/s654632396/doublelist => /mnt/d/env/go/src/s654632396/pkg/doublelist
+
+// replace github.com/s654632396/pkg/adt => /mnt/d/env/go/src/s654632396/pkg/adt
---
+++ test_btree.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"github.com/s654632396/pkg/adt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	var bt = adt.NewBTree()
+	var items []adt.NodeValue = []adt.NodeValue{29, 1, 35, 32, 11, 34, 54, 12, 2, 5, 9, 6, 45, 17, 33}
+	for _, item := range items {
+		bt.Add(adt.NodeValue(item), item+10000)
+	}
+	var seed int = time.Now().Nanosecond()
+	rand.Seed(int64(seed))
+	// for i := 0; i < 80; i++ {
+	// 	value := NodeValue(rand.Intn(100))
+	// 	bt.Add(value, value+100000)
+	// }
+
+	var list []adt.NodeValue
+	var f1 = func(n *adt.Node) {
+		list = append(list, n.Value())
+	}
+	// bt.InOrderTraversal(f1)
+	// fmt.Printf("in-order traverse binary tree -----------------------------\n%#v\n", list)
+
+	bt.String()
+	unlink := bt.Find(29)
+	if unlink != nil {
+		unlinked := bt.Unlink(unlink)
+		fmt.Printf("SUCCESSFULLY UNLINKED NODE: %d\n", unlinked)
+		bt.String()
+		bt.InOrderTraversal(f1)
+		// fmt.Printf("in-order traverse binary tree -----------------------------\n%#v\n", list)
+	}
+
+	return
+}
